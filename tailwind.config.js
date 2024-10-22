fee8f25ceb6ba795794c935ef81da0c41609d39b
André Miyazawa <andre08.m@usp.br>
2024-10-21 22:35:28 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  theme: {
+    extend: {
+      borderColor: {
+        'custom-gray': '#3A3A3C'
+      },
+      backgroundColor: {
+        'custom-light-gray': 'rgb(129 131 132)'
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
